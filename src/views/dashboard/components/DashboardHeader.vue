f65cc78051415ff14aa8fb50dfcfbfa68a2a6c4f
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-10-20 01:30:51 -0300

    Create the component DashboardHearder

---
+++ src/views/dashboard/components/DashboardHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="dashboard-header">
+        <h1>Dashboard</h1>
+    </header>
+</template>
+
+<style lang="stylus" scoped>
+height = 50px
+
+.dashboard-header
+    width 100vw
+    height height
+    line-height height
+    background-color #fff   
+    border-bottom 2px solid #e4e8f0
+
+    h1
+        margin 0
+</style>
