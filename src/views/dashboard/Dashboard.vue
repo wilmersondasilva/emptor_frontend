c8b832408dd239170eccf29f8d9a739a4f622b9d
Wilmerson da Silva <wilmersondasilva@gmail.com>
2019-10-20 01:31:03 -0300

    Create the component Dashboard

---
+++ src/views/dashboard/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="dashboard">
+        <DashboardHeader />
+    </div>
+</template>
+
+<script>
+import DashboardHeader from './components/DashboardHeader.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        DashboardHeader,
+    },
+}
+</script>
+
+<style lang="stylus">
+.dashboard
+    height 100vh
+    background-color #f4f6fc
+</style>
